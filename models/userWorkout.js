252fa16d1a8404a149de4432ccd8bcb77acb4695
jenry valdes <jenryval93@gmail.com>
2021-03-15 20:48:05 -0500

    rearrange stuff

---
+++ models/userWorkout.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.schema
+
+const Exercises = new Schema({
+    type: {
+        type: String,
+        trim: true,
+        required: [true, 'Enter the type of workout']
+   },
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Enter the name of the workout']
+    },
+    duration: {
+        type: Number,
+        trim: true,
+        required: [true, 'Enter the duration of the workout']
+    },
+    weight: {
+        type: Number,
+        trim: true
+    },
+    reps: {
+        type: Number,
+        trim: true
+    },
+    sets: {
+        type: Number,
+        trim: true
+    },
+    distance: {
+        type: Number,
+        trim: true,
+    }
+})
+
+const workoutschema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [Exercises]
+});
+
+
+const Workout = mongoose.model('Workout', workoutschema)
+
+module.exports = Workout
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongojs = require('mongojs');
+const { Mongoose } = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});

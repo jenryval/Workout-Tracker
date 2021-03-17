1cf95801b2690d9f17799256189a4af890890cce
jenry valdes <jenryval93@gmail.com>
2021-03-16 22:02:35 -0500

    added routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,25 +1,59 @@
 const Workout = require("../models/userWorkout");
 const router = require("express").Router();
+// const db = require('')
 
-
-router.get('/api/workouts', (res, res) => {
-    Workout.find()
-    .then(dbWorkout => {
-        res.json(dbWorkout)
+// GET last workout
+router.get('/api/workouts', (req, res) => {
+    Workout.find(req)
+    .then(workouts => {
+        res.json(workouts)
     })
     .catch(err => {
         res.status(400).json(err)
     })
 });
 
-router.post('/api/workouts', (req,res) => {
-    Workout.create({})
-    .then(dbWorkout => res.json(dbWorkout))
+
+// GET workouuts in range
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggragate([
+        {
+            $addFields: {
+                totalDuration: {$sum: "$duration"}
+            }
+        }
+    ])
+    .then(workouts => {
+        res.json(workouts)
+    })
     .catch(err => {
+        res.status(400).json(err)
+    })
+});
+
+
+// PUT add exercise
+router.put('/api/wokouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(
+        params.if,
+        {$push: {exercises:body} },
+
+        {new: true, runValidators: true}
+    ).then(workouts => {
+        res.json(workouts);
+    }).catch(err => {
         res.json(err)
     })
 });
 
 
+// POST create workout
+router.post('/api/workouts', (req , res) => {
+    Workout.create({})
+    .then(workouts => res.json(workouts))
+    .catch(err => {
+        res.json(err)
+    })
+});
 
 module.exports = router;

7d2619e303b09279c33a02f311bda47df40f64e1
jenry valdes <jenryval93@gmail.com>
2021-03-22 21:45:25 -0500

    Heroku

--- public/exercise.js
+++ public/exercise.js
@@ -21,12 +21,10 @@ async function initExercise() {
   let workout;
 
   if (location.search.split("=")[1] === 'undefined') {
-    console.log("hello world")
     workout = await API.createWorkout()
     console.log(workout)
   }
   if (workout) {
-    console.log("hello world")
     location.search = "?id=" + workout._id;
   }
 
